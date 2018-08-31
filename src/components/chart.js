bb26c30fc3bfd730f2406d2c0c961e2ad75d8293
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-08-30 22:53:56 -0400

    added referenceline

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Sparklines, SparklinesBars } from 'react-sparklines';
+import _ from 'lodash';
+import { Sparklines, SparklinesBars, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data){
+  return _.round(_.sum(data)/data.length);
+}
 
 export default (props) => {
   return(
     <div>
     <Sparklines data={props.data}>
       <SparklinesBars color={props.color}/>
+      <SparklinesReferenceLine type="mean" />
     </Sparklines>
+    //for displaying average data//
+    <div>{average(props.data)}</div>
     </div>
   );
 }
